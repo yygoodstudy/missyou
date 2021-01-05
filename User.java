df10954376a722ac97b3d3eb0893857708d6ed65
yygoodstudy <50063355+yygoodstudy@users.noreply.github.com>
2021-01-05 23:27:52 +0800

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,21 @@
 
+package com.yy.ymissyou.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * @Date: 2021/1/4 22:53
+ * @Description:
+ */
+@Getter
+@Setter
+@ToString
+public class User {
+
+    @Length(min = 2,max = 20,message = "长度应该介于2和20之间")
+    private String schoolName;
+
+}
